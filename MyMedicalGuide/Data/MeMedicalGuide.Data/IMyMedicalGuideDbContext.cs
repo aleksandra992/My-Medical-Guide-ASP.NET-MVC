1a407dc2f8e4a3b350d6c9dee5c7efefcec67f19
Aleksandra Stojcheva <astojceva@yahoo.com>
2016-02-03 14:43:39 +0200

    Skeleton Added

---
+++ MyMedicalGuide/Data/MeMedicalGuide.Data/IMyMedicalGuideDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace MeMedicalGuide.Data
+{
+    public interface IMyMedicalGuideDbContext
+    {
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        void Dispose();
+
+        int SaveChanges();
+    }
+}
---
+++ MyMedicalGuide/Data/MeMedicalGuide.Data/MyMedicalGuideDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace MeMedicalGuide.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using MyMedicalGuide.Data.Models;
+
+    public class MyMedicalGuideDbContext : IdentityDbContext<User>
+    {
+        public MyMedicalGuideDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MyMedicalGuideDbContext Create()
+        {
+            return new MyMedicalGuideDbContext();
+        }
+    }
+}
